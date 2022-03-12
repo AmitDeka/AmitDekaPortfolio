e38a4d4749defdd864644d6bfad798c55a90e8cb
AmitDeka <amitdeka49@gmail.com>
2022-03-12 23:37:21 +0530

    Update Portfolio Website

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        IBMplexSans: ["IBM Plex Sans", "sans-serif"],
+        IBMplexSerif: ["IBM Plex Serif", "serif"],
+      },
+      colors: {
+        green: "#64ffda",
+        lightBlue: "#ccd6f6",
+        gray: "#8892b0",
+        navy: "#0a192f",
+        navyL: "#112240",
+      },
+    },
+  },
+  plugins: [],
+};
