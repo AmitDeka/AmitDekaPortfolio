4e7b222e20cde4c3070688fbd2e9e50dbc5de78c
AmitDeka <amitdeka49@gmail.com>
2022-04-28 23:05:26 +0530

    Update 28 Apr *Added GSAP

---
+++ src/assets/js/animation.js
@@ -0,0 +1,43 @@
+gsap.from(".home", {
+  duration: 0.8,
+  delay: 0.3,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.from(".about", {
+  duration: 0.8,
+  delay: 0.45,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.from(".experience", {
+  duration: 0.8,
+  delay: 0.6,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.from(".work", {
+  duration: 0.8,
+  delay: 0.75,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.from(".contact", {
+  duration: 0.8,
+  delay: 0.9,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.from(".resume_btn", {
+  duration: 0.8,
+  delay: 1,
+  opacity: 0,
+  y: "-50px",
+  ease: Power1.easeInOut,
+});
+gsap.registerPlugin(ScrollTrigger);
